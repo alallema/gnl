ea480ace4f4dde6ea1294f03e9c6f2fd1b43b063
Amelie LALLEMAND <alallema@e2r7p4.42.fr>
2016-03-18 20:39:47 +0100

    nettoyage

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,15 @@
 /*   By: alallema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/18 17:14:56 by alallema          #+#    #+#             */
-/*   Updated: 2016/03/18 19:49:43 by alallema         ###   ########.fr       */
+/*   Updated: 2016/03/18 20:05:58 by alallema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft.h"
 
 char	*ft_clean(char *s)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	s = ft_strdup(s);
@@ -26,61 +25,50 @@ char	*ft_clean(char *s)
 		s[i] = '\0';
 		i++;
 	}
-	return(s);
+	return (s);
 }
 
 char	*ft_strcchr(char *s)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	s = ft_strdup(s);
-	while(s[i] != '\n' && s[i])
+	while (s[i] != '\n' && s[i])
 		i++;
 	i++;
-		return(&s[i]);
+	return (&s[i]);
 }
 
-int	get_next_line(const int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
 	char		buf[BUFF_SIZE + 1];
 	static char	*tmp;
 	int			ret;
-	char		*s;
 
 	if (!tmp)
 		tmp = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
 	if (fd == -1)
-		return(-1);
+		return (-1);
 	if (ft_strchr(tmp, '\n'))
 	{
-		ft_putchar('t');
-		ft_putchar('\n');
 		*line = ft_clean(tmp);
 		tmp = ft_strcchr(tmp);
 		return (1);
 	}
-	while ((ret = read (fd, &buf, BUFF_SIZE)) != 0)
+	while ((ret = read(fd, &buf, BUFF_SIZE)) != 0)
 	{
 		buf[ret] = '\0';
-//		tmp = ft_strjoin(tmp, buf);
 		if (!ft_strchr(buf, '\n'))
 		{
-			ft_putchar('a');
-			ft_putchar('\n');
 			tmp = ft_strjoin(tmp, buf);
-//			ft_putstr(tmp);
 		}
 		if (ft_strchr(buf, '\n'))
 		{
-//			ft_putstr(buf);
-			ft_putchar('b');
-			ft_putchar('\n');
 			*line = ft_strjoin(tmp, ft_clean(buf));
 			tmp = ft_strcchr(buf);
-//			ft_putstr(tmp);
 			return (1);
 		}
 	}
-	return(0);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,21 @@
 /*   By: alallema <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/09 17:59:29 by alallema          #+#    #+#             */
-/*   Updated: 2016/03/12 15:10:12 by alallema         ###   ########.fr       */
+/*   Updated: 2016/03/18 20:30:37 by alallema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+#include "libft/includes/libft.h"
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
 #include <stdlib.h>
-# define BUFF_SIZE	32
+
+# define BUFF_SIZE	8
 
 int	get_next_line(const	int fd, char	**line);
 
