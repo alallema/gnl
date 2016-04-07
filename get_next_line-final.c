c84ac51e567d7ee4eb1bec2dd718ddc5026f73ff
alallema <alallema@users.noreply.github.com>
2016-04-07 10:16:28 +0200

    Added files via upload

---
+++ get_next_line-final.c
@@ -0,0 +1,58 @@
+#include "get_next_line.h"
+
+static	char	*ft_fix_join(char *s, char *buf)
+{
+	char	*tmp;
+
+	tmp = s;
+	s = ft_strjoin(tmp, buf);
+	free(tmp);
+	return (s);
+}
+char			*ft_check_stock(char *tmp)
+{
+	int		i;
+	char		*s;
+
+	i = 0;
+	while (tmp[i] && tmp[i] != '\n')
+		i++;
+	s = ft_strsub(tmp, 0, i);
+	ft_strcpy(tmp, &tmp[i + 1]);
+	return (s);
+}
+
+int			ft_read_fd(const int fd, char **tmp)
+{
+	char		buf[BUFF_SIZE + 1];
+	int		ret;
+
+	while ((ret = read(fd, &buf, BUFF_SIZE)) > 0)
+	{
+		buf[ret] = '\0';
+		*tmp = ft_fix_join(*tmp, buf);
+		if (ft_strchr(*tmp, '\n'))
+			return (ret);
+	}
+	return (ret);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	static char	*tmp;
+	int		ret;
+
+	if (fd < 0 || !line || BUFF_SIZE < 0)
+		return (-1);
+	if (!tmp)
+		tmp = ft_strnew(BUFF_SIZE);
+	if ((ret = ft_read_fd(fd, &tmp)) < 0)
+		return (-1);
+	if (ret == 0 && tmp[0] == '\0')
+	{
+		ft_strdel(line);
+		return (0);
+	}
+	*line = ft_check_stock(tmp);
+	return (1);
+}
